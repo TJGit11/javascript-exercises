c40cbe8eaddea0cc0f2afff7901453c0f9c90425
TJ <thomasjjohnson9311@gmail.com>
2022-05-20 07:27:24 -0500

    exercise 03 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    // split the string into an array [h, e, l, l, o]
+    let splitString = str.split("");
+    // reverse the array [o, l, l, e, h]
+    let reversed = splitString.reverse();
+    // join the array back together "olleh"
+    let joined = reversed.join("");
+    return joined;
 
 };
 
