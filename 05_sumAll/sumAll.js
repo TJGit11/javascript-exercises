4b414c0f876ade96259c736f6fd11f4f6bba528c
TJ <thomasjjohnson9311@gmail.com>
2022-05-26 11:54:33 -0500

    05 exercise complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+if (x < 0 || y < 0) return 'ERROR';
+if (!Number.isInteger(x) || (!Number.isInteger(y))){
+    return 'ERROR';
+}
 
-};
+if (x > y) {
+    let j = x
+    x = y
+    y = j
+}
+
+let sumxy = 0;
+for (let i = x; i < y + 1; i++){
+    sumxy += i;
+}
+return sumxy;
+}
+
 
 // Do not edit below this line
 module.exports = sumAll;
